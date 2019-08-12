c43494838a4245b4b951d74155198e28c95e711a
shyamravikumar6 <shyamravikumar6@gmail.com>
2019-08-12 20:05:57 +0530

    login_page added

---
+++ flutter/trackig_services/lib/login/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  createState() => Loginpage();
+}
+
+class Loginpage extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("bitch a bop"),),
+    );
+  }
+}
